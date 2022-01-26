a7df33efdcd4ecda6a474bbdbf47ba81539919e5
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-01-26 17:00:47 +0700

    Update triplet_loss.py

--- new_model/triplet_loss.py
+++ new_model/triplet_loss.py
@@ -1,30 +1,80 @@
 """Define functions to create the triplet loss with online triplet mining."""
-# https://github.com/phongdinhv/triplet-loss-keras-mnist/blob/master/triplet_loss.ipynb
-
+# https://github.com/AdrianUng/keras-triplet-loss-mnist/blob/master/Triplet_loss_KERAS_semi_hard_from_TF.ipynb
 import tensorflow as tf
 import numpy as np
 from keras import backend as K
 
 def triplets_loss(y_true, y_pred):
-    embeddings = y_pred
-    anchor_positive = embeddings[:10]
-    negative = embeddings[10:]
-    
-    # Compute pairwise distance between all of anchor-positive
-    dot_product = K.dot(anchor_positive, K.transpose(anchor_positive))
-    square = K.square(anchor_positive)
-    a_p_distance = K.reshape(K.sum(square, axis=1), (-1,1)) - 2.*dot_product  + K.sum(K.transpose(square), axis=0) + 1e-6
-    a_p_distance = K.maximum(a_p_distance, 0.0) ## Numerical stability
+    del y_true
+    margin = 1.
+    labels = y_pred[:, :1]
 
-    # Compute distance between anchor and negative
-    dot_product_2 = K.dot(anchor_positive, K.transpose(negative))
-    negative_square = K.square(negative)
-    a_n_distance = K.reshape(K.sum(square, axis=1), (-1,1)) - 2.*dot_product_2  + K.sum(K.transpose(negative_square), axis=0)  + 1e-6
-    a_n_distance = K.maximum(a_n_distance, 0.0) ## Numerical stability
+ 
+    labels = tf.cast(labels, dtype='int32')
+
+    embeddings = y_pred[:, 1:]
+
+    ### Code from Tensorflow function [tf.contrib.losses.metric_learning.triplet_semihard_loss] starts here:
     
-    hard_negative = K.reshape(K.min(a_n_distance, axis=1), (-1, 1))
+    # Reshape [batch_size] label tensor to a [batch_size, 1] label tensor.
+    # lshape=array_ops.shape(labels)
+    # assert lshape.shape == 1
+    # labels = array_ops.reshape(labels, [lshape[0], 1])
+
+    # Build pairwise squared distance matrix.
+    pdist_matrix = pairwise_distance(embeddings, squared=True)
+    # Build pairwise binary adjacency matrix.
+    adjacency = math_ops.equal(labels, array_ops.transpose(labels))
+    # Invert so we can select negatives only.
+    adjacency_not = math_ops.logical_not(adjacency)
+
+    # global batch_size  
+    batch_size = array_ops.size(labels) # was 'array_ops.size(labels)'
+
+    # Compute the mask.
+    pdist_matrix_tile = array_ops.tile(pdist_matrix, [batch_size, 1])
+    mask = math_ops.logical_and(
+        array_ops.tile(adjacency_not, [batch_size, 1]),
+        math_ops.greater(
+            pdist_matrix_tile, array_ops.reshape(
+                array_ops.transpose(pdist_matrix), [-1, 1])))
+    mask_final = array_ops.reshape(
+        math_ops.greater(
+            math_ops.reduce_sum(
+                math_ops.cast(mask, dtype=dtypes.float32), 1, keepdims=True),
+            0.0), [batch_size, batch_size])
+    mask_final = array_ops.transpose(mask_final)
+
+    adjacency_not = math_ops.cast(adjacency_not, dtype=dtypes.float32)
+    mask = math_ops.cast(mask, dtype=dtypes.float32)
+
+    # negatives_outside: smallest D_an where D_an > D_ap.
+    negatives_outside = array_ops.reshape(
+        masked_minimum(pdist_matrix_tile, mask), [batch_size, batch_size])
+    negatives_outside = array_ops.transpose(negatives_outside)
+
+    # negatives_inside: largest D_an.
+    negatives_inside = array_ops.tile(
+        masked_maximum(pdist_matrix, adjacency_not), [1, batch_size])
+    semi_hard_negatives = array_ops.where(
+        mask_final, negatives_outside, negatives_inside)
+
+    loss_mat = math_ops.add(margin, pdist_matrix - semi_hard_negatives)
+
+    mask_positives = math_ops.cast(
+        adjacency, dtype=dtypes.float32) - array_ops.diag(
+        array_ops.ones([batch_size]))
+
+    # In lifted-struct, the authors multiply 0.5 for upper triangular
+    #   in semihard, they take all positive pairs except the diagonal.
+    num_positives = math_ops.reduce_sum(mask_positives)
+
+    semi_hard_triplet_loss_distance = math_ops.truediv(
+        math_ops.reduce_sum(
+            math_ops.maximum(
+                math_ops.multiply(loss_mat, mask_positives), 0.0)),
+        num_positives,
+        name='triplet_semihard_loss')
     
-    distance = (a_p_distance - hard_negative + 0.2)
-    loss = K.mean(K.maximum(distance, 0.0))/(2.)
-            
-    return loss
+    ### Code from Tensorflow function semi-hard triplet loss ENDS here.
+    return semi_hard_triplet_loss_distance
