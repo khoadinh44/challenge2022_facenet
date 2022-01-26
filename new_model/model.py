0d704497407a5000ed911974ccb1bbd082f598f6
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-01-26 10:21:32 +0700

    Update model.py

--- new_model/model.py
+++ new_model/model.py
@@ -1,23 +1,36 @@
 import os
 import tensorflow as tf
-import keras
+import keras.models
+from keras import backend as K
+from keras.layers import Input, Concatenate
+from keras.models import Model
+    
+def get_model(n_frames, n_mels, n_conditions, lr):
+    # InceptionV3 is chosen for the base model##############################################
+    base_model =  tf.keras.applications.InceptionV3(include_top=False,
+                                                   input_shape=(n_frames, n_mels, 3),
+                                                   weights=None,
+                                                   classes=n_conditions)
+#     base_model.trainable = False
+    
+    # The input layer have the shape of 3-Dimention (64, 128, 3)############################
+    x = Input(shape=(n_frames, n_mels, 1))
+    h = x
+    h = Concatenate()([h, h, h])
+    
+    # The final model#######################################################################
+    h = base_model(input_layer)
+    model = Model(x, h)
+    model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr), 
+              loss='categorical_crossentropy', 
+              metrics=['accuracy'])
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+return model
 
+#########################################################################
 
-class face_model(tf.keras.Model): 
-    def __init__(self, params):
-        super(face_model, self).__init__()
-        img_size = (params.image_size, params.image_size, 3)
-        self.base_model = tf.keras.applications.InceptionV3(include_top=False, input_shape=img_size)
-        self.base_model.trainable = False
-        self.flatten = tf.keras.layers.Flatten()
-        self.embedding_layer = tf.keras.layers.Dense(units=params.embedding_size)
-        
-    def call(self, images):
-        x = self.base_model(images)
-        x = self.flatten(x)
-        x = self.embedding_layer(x)
-        # return tf.keras.Model(tf.keras.layers.Input([128, 128, 3]), self.embedding_layer)
-        return x
+def load_model(file_path):
+    return keras.models.load_model(file_path, compile=False)
+
+def clear_session():
+    K.clear_session()
