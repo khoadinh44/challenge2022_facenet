1445af82109dfb88e5a801a6b03d218c686e8ceb
khoadinh44 <66673977+khoadinh44@users.noreply.github.com>
2022-01-26 11:28:04 +0700

    Update model.py

--- new_model/model.py
+++ new_model/model.py
@@ -9,8 +9,7 @@ def get_model(n_frames, n_mels, n_conditions, lr):
     # InceptionV3 is chosen for the base model##############################################
     base_model =  tf.keras.applications.MobileNetV3Large(include_top=False,
                                                    input_shape=(n_frames, n_mels, 3),
-                                                   weights=None,
-                                                   classes=n_conditions)
+                                                   weights=None)
 #     base_model.trainable = False
     
     # The input layer have the shape of 3-Dimention (64, 128, 3)############################
@@ -20,7 +19,11 @@ def get_model(n_frames, n_mels, n_conditions, lr):
     
     # The final model#######################################################################
     h = base_model(h)
-    model = Model(x, h)
+    flatten = tf.keras.layers.Flatten()(h)
+    embedding_layer = tf.keras.layers.Dense(units=n_conditions)(flatten)
+    model = Model(x, embedding_layer)
+    
+    # Compile###############################################################################
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr), 
               loss='categorical_crossentropy', 
               metrics=['accuracy'])
